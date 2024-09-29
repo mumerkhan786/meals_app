9dff70617c028ed253cffc2406dfc596fd3aebab
mumerkhan786 <muhammadumerkhan224@gmail.com>
2024-09-29 05:10:59 -0700

    Updated Category_grid_items Widget and added logic in it and pass them to categories screen

---
+++ lib/widgets/category_grid_items.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryGridItems extends StatelessWidget {
+  const CategoryGridItems({
+    super.key,
+    required this.category,
+  });
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(0.9),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Column(
+        children: [
+          Text(
+            category.title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+}
