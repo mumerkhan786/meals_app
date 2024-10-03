42198703bf797b044246e662b97bb401077942da
mumerkhan786 <muhammadumerkhan224@gmail.com>
2024-10-03 13:07:21 -0700

    Added new filter screen

---
+++ lib/screens/filters.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:meals/widgets/filter_item.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFreeFilterSet = false;
+  var _lactoseFreeFilterSet = false;
+  var _vegetarianFilterSet = false;
+  var _veganFilterSet = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      body: Column(
+        children: [
+          FilterItem(
+            value: _glutenFreeFilterSet,
+            mainTitle: 'Gluten-Free',
+            subTitle: 'Only include gluten-free meals.',
+            onChanged: (isChecked) {
+              setState(
+                () {
+                  _glutenFreeFilterSet = isChecked;
+                },
+              );
+            },
+          ),
+          FilterItem(
+            value: _lactoseFreeFilterSet,
+            mainTitle: 'Lactose-Free',
+            subTitle: 'Only include lactose-free meals.',
+            onChanged: (isChecked) {
+              setState(
+                () {
+                  _lactoseFreeFilterSet = isChecked;
+                },
+              );
+            },
+          ),
+          FilterItem(
+            value: _vegetarianFilterSet,
+            mainTitle: 'Vegetarian',
+            subTitle: 'Only include vegetarian meals.',
+            onChanged: (isChecked) {
+              setState(
+                () {
+                  _vegetarianFilterSet = isChecked;
+                },
+              );
+            },
+          ),
+          FilterItem(
+            value: _veganFilterSet,
+            mainTitle: 'Vegan',
+            subTitle: 'Only include vegan meals.',
+            onChanged: (isChecked) {
+              setState(
+                () {
+                  _veganFilterSet = isChecked;
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
