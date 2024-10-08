bef9a814a5b567faf4523c5f66c48f9b67707c1b
mumerkhan786 <muhammadumerkhan224@gmail.com>
2024-10-08 12:09:55 -0700

    Removed unncessory comments

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -6,26 +6,6 @@ import 'package:meals/providers/filters_provider.dart';
 class FiltersScreen extends ConsumerWidget {
   const FiltersScreen({super.key});
 
-//   @override
-//   ConsumerState<FiltersScreen> createState() => _FiltersScreenState();
-// }
-
-// class _FiltersScreenState extends ConsumerState<FiltersScreen> {
-//   var _glutenFreeFilterSet = false;
-//   var _lactoseFreeFilterSet = false;
-//   var _vegetarianFilterSet = false;
-//   var _veganFilterSet = false;
-
-//   @override
-//   void initState() {
-//     final activeFilters = ref.read(filtersProvider);
-//     _glutenFreeFilterSet = activeFilters[Filters.glutenFree]!;
-//     _lactoseFreeFilterSet = activeFilters[Filters.lactoseFree]!;
-//     _vegetarianFilterSet = activeFilters[Filters.vegetarian]!;
-//     _veganFilterSet = activeFilters[Filters.vegan]!;
-//     super.initState();
-//   }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final activeFilters = ref.watch(filtersProvider);
@@ -33,19 +13,7 @@ class FiltersScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
-      body:
-          // PopScope(
-          //   canPop: true,
-          //   onPopInvokedWithResult: (didPop, result) {
-          //     ref.read(filtersProvider.notifier).setFilters({
-          //       Filters.glutenFree: _glutenFreeFilterSet,
-          //       Filters.lactoseFree: _lactoseFreeFilterSet,
-          //       Filters.vegetarian: _vegetarianFilterSet,
-          //       Filters.vegan: _veganFilterSet,
-          //     });
-          //   },
-          //   child:
-          Column(
+      body: Column(
         children: [
           FilterItem(
             value: activeFilters[Filters.glutenFree]!,
